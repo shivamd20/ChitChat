7c56041f2a49f4f3fd16fdb9d02df3f704a60392
Shivam Dwivedi <shivamd20@gmail.com>
2017-07-03 14:29:01 +0530

    Solved greendao error

--- app/src/main/java/io/hasura/shivam/chitchat/CustApplication.java
+++ app/src/main/java/io/hasura/shivam/chitchat/CustApplication.java
@@ -5,16 +5,24 @@ import android.content.res.Configuration;
 import android.widget.Toast;
 
 
+import org.greenrobot.greendao.database.Database;
+
 import io.hasura.sdk.Hasura;
 import io.hasura.sdk.ProjectConfig;
 import io.hasura.sdk.exception.HasuraInitException;
+import io.hasura.shivam.chitchat.db.DaoMaster;
+import io.hasura.shivam.chitchat.db.DaoSession;
 
 public class CustApplication extends Application {
 
     // Called when the application is starting, before any other application objects have been created.
     // Overriding this method is totally optional!
 
-  //TODO  protected AbstractDaoSession daoSession;
+
+    DaoSession daoSession;
+
+
+    //TODO  protected AbstractDaoSession daoSession;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -31,16 +39,28 @@ public class CustApplication extends Application {
                     .enableLogs() // not included by default
                     .initialise(this);
 
-        }
-        catch (HasuraInitException hie)
-        {
-            Toast.makeText(this,hie.getMessage(),Toast.LENGTH_SHORT).show();
+        } catch (HasuraInitException hie) {
+            Toast.makeText(this, hie.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "chitchat-db"); //The users-db here is the name of our database.
+        Database db = helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+
+        ///// Using the below lines of code we can toggle ENCRYPTED to true or false in other to use either an encrypted database or not.
+//      DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, ENCRYPTED ? "users-db-encrypted" : "users-db");
+//      Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
+//      daoSession = new DaoMaster(db).newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
 
 
         // Required initialization logic here!
-    }
+
 
     // Called by the system when the device configuration changes while your component is running.
     // Overriding this method is totally optional!
